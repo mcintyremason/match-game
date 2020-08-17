6d0bc5da2dc26b9c42d52becf5c7b5060df6eaf4
mcintyremason <mcintyre.mlm@gmail.com>
2020-08-16 22:38:46 -0400

    create-react-app

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import * as React from "react";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+
+import HomePage from "./components/HomePage/index";
+import "./css/global.css";
+import "./css/fireworks.css";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#58b2fd",
+      contrastText: "#ffffff",
+    },
+  },
+  props: {
+    MuiTypography: {
+      variantMapping: {
+        h1: "h1",
+        h2: "h2",
+        h3: "h3",
+        h4: "h4",
+        h5: "h5",
+        h6: "h6",
+        subtitle1: "h2",
+        subtitle2: "h2",
+        body1: "span",
+        body2: "span",
+      },
+    },
+  },
+});
+
+const App = () => (
+  <ThemeProvider theme={theme}>
+    <HomePage />
+  </ThemeProvider>
+);
+
+export default App;
+
+// ReactDOM.render(<App />, document.getElementById('app'))
--- src/index.tsx
+++ src/index.tsx
@@ -1,44 +1,17 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
 
-import HomePage from './components/HomePage/index'
-import './styles/css/global.css'
-import './styles/css/fireworks.css'
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root"),
+);
 
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: '#58b2fd',
-      contrastText: '#ffffff',
-    },
-  },
-  props: {
-    MuiTypography: {
-      variantMapping: {
-        h1: 'h1',
-        h2: 'h2',
-        h3: 'h3',
-        h4: 'h4',
-        h5: 'h5',
-        h6: 'h6',
-        subtitle1: 'h2',
-        subtitle2: 'h2',
-        body1: 'span',
-        body2: 'span',
-      },
-    },
-  },
-})
-
-export default class App extends React.Component {
-  render() {
-    return (
-      <ThemeProvider theme={theme}>
-        <HomePage />
-      </ThemeProvider>
-    )
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById('app'))
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
